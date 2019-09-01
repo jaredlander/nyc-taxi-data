48e81f0a3db73958ef96626f3c32538d24cbd2c9
Jared P. Lander <jared@landeranalytics.com>
2019-09-01 14:20:24 -0400

    Deleting old data before inserting new data

--- import_fake_cab_data.sh
+++ import_fake_cab_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# delete old data
+psql -h 172.27.168.233 -p 5434 -U docker nyc-taxi-data -c "DELETE FROM fake_cabs;"
+
 fake_cab_schema="(id,cabid,longitude,latitude)"
 cat data/fake_cab_info.csv | psql -h 172.27.168.233 -p 5434 -U docker nyc-taxi-data -c "COPY fake_cabs ${fake_cab_schema} FROM stdin CSV HEADER;"
 
